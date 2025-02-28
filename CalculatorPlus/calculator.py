cf1b55ab28a03624cdd024dce97605b75061fffc
amitjaswal28 <amitjaswal08@gmail.com>
2025-02-28 23:48:25 +0530

    bug fixation

--- CalculatorPlus/calculator.py
+++ CalculatorPlus/calculator.py
@@ -11,7 +11,10 @@ class Calculator:
     def multiply(self, a, b):
         return a * b
 
+   #The bug fixation is in the divide function and the new function should be: 
     def divide(self, a, b):
+        if b == 0:
+            raise ValueError("Cannot divide by zero.")
         return a / b
   
     
