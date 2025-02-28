cffb4aab829293ffbbc2e66e52c9ae4d6abebdc7
amitjaswal28 <amitjaswal08@gmail.com>
2025-03-01 01:30:22 +0530

    rectangle area complete code

--- CalculatorPlus/geometryCalculator.py
+++ CalculatorPlus/geometryCalculator.py
@@ -9,3 +9,9 @@ class GeometryCalculator:
 
 if __name__ == "__main__":
     calculator = GeometryCalculator()
+   
+   # TODO: Implement the feature to calculate the area of a rectangle # length = 10
+
+    width = 6
+    length=8
+    print(f"The area of the rectangle with length {length} and width {width} = {calculator.calculate_rectangle_area(length, width)}")
